f66c75108dddca852d42d1d1567f0f8fbe95f1ad
daeho <679748@naver.com>
2025-05-19 20:20:11 +0900

    기초 데이터 init 로그

---
+++ src/test/kotlin/com/kotlinprc/accountapi/AccountApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinprc.accountapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
