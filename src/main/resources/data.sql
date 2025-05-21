21f987ddb48155aaec8e44f45db32a38bb97c3ae
daeho <679748@naver.com>
2025-05-21 21:08:22 +0900

    테스트값 추가
    응닶
    {
      "userId": 1,
      "accountNumber": "1000000000",
      "registerAt": "2025-05-21T21:07:55.2016826"
    }

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into account_user (id,name,created_at,updated_at) values(1,'test1', now(), now());
+insert into account_user (id,name,created_at,updated_at) values(2,'test2', now(), now());
+insert into account_user (id,name,created_at,updated_at) values(3,'test3', now(), now());
+insert into account_user (id,name,created_at,updated_at) values(4,'test4', now(), now());
+insert into account_user (id,name,created_at,updated_at) values(5,'test5', now(), now());
