c3fdab718bfca6f351c6b5a1767717655078e72c
daeho <679748@naver.com>
2025-05-22 19:38:11 +0900

    ErrorCode 정의

--- src/main/kotlin/com/kotlinprc/accountapi/model/enums/ErrorCode.kt
+++ src/main/kotlin/com/kotlinprc/accountapi/model/enums/ErrorCode.kt
@@ -1,5 +1,9 @@
 package com.kotlinprc.accountapi.model.enums
 
 enum class ErrorCode(val description: String) {
-    USER_NOT_FOUND("사용자가 없습니다.")
+    USER_NOT_FOUND("사용자가 없습니다."),
+    ACCOUNT_NOT_FOUND("계좌가 없습니다."),
+    USER_ACCOUNT_UN_MATCH("사용자와 계좌의 소유주가 다릅니다."),
+    ACCOUNT_ALREADY_UNREGISTERED("계좌가 이미 혜지되었습니다."),
+    BALANCE_NOT_EMPTY("잔액이 있는 계좌는 혜지 할 수 없습니다."),
 }
