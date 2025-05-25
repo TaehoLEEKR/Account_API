fce9b32a08d5107178e8cb5bc60668564784961e
TaehoLEEKR <679748@naver.com>
2025-05-25 15:17:26 +0900

    Transaction Entity 객체 DB 생성

---
+++ src/main/kotlin/com/kotlinprc/accountapi/model/entity/Transaction.kt
@@ -0,0 +1,40 @@
+package com.kotlinprc.accountapi.model.entity
+
+import com.kotlinprc.accountapi.model.enums.TransactionResult
+import com.kotlinprc.accountapi.model.enums.TransactionType
+import jakarta.persistence.*
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@Entity
+@EntityListeners(AuditingEntityListener::class)
+open class Transaction(
+    @Enumerated(EnumType.STRING)
+    open var transactionType: TransactionType = TransactionType.NOT_USE,
+
+    @Enumerated(EnumType.STRING)
+    open var transactionResult: TransactionResult = TransactionResult.F,
+
+    @ManyToOne
+    open var account: Account? = null,
+
+    open var balanceSnapshot : Long = 0L,
+
+    open var transactionId : Long = 0L,
+    open var transactionAt : LocalDateTime = LocalDateTime.now(),
+
+    @CreatedDate
+    open var createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @LastModifiedDate
+    open var updatedAt: LocalDateTime = LocalDateTime.now()
+
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    open var id: Long? = null
+
+}
